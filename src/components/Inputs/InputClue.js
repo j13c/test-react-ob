9a730ab6c1ed759ce9f01ccdb392aad228373cef
jtrujillo <jtrujillo@cgtscorp.com>
2020-11-04 10:52:13 -0400

    add: contador de caracteres en input

--- src/components/Inputs/InputClue.js
+++ src/components/Inputs/InputClue.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Images} from './../../constant/image';
 
 function InputClue(props) {
 
-  let myRef = React.createRef();
+  const [cantChar, setCantChar] = useState(0)
   const {title, placeholderTxt, lengthClue} = props;
   const icon = Images.iconInfo;
-  let cantChar = lengthClue;
 
+  const handleValue = (event) => {
+    const value = event.target.value;
+    setCantChar(String(value).length);
+  }
 
   return (
     <div id="input-pista" className="item-container-input-clue">
       <label>{title} <img src={icon.src} alt={icon.alt} height={icon.height} width={icon.width} ></img></label>
       <input  type="text" 
               placeholder={placeholderTxt} 
-              ref={myRef} ></input>
+              onKeyUp={ handleValue }></input>
       <span>{cantChar}/60</span>
     </div>
   )
